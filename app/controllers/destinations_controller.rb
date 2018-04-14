3aa117453101e47d44afcc0f76fe56a07a9a7796
Chris Henry <cdhenry@gmail.com>
2018-04-14 14:55:34 +0000

    created starting controller for destinations

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,49 @@
+class DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @destinations = Destination.all
+  end
+
+  def show
+  end
+
+  def new
+    @destination = Destination.new
+  end
+
+  def edit
+  end
+
+  def create
+    @destination = Destination.new(destination_params)
+
+    if @destination.save
+      redirect_to @destination
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @destination.update(destination_params)
+      redirect_to @destination
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @destination.destroy
+    redirect_to destinations_url
+  end
+
+  private
+    def set_destination
+      @destination = Destination.find(params[:id])
+    end
+
+    def destination_params
+      params.require(:destination).permit(:name, :description, :city, :state, :street_address, tags_attributes: [:title])
+    end
+end
