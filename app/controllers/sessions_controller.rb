7876dc924a3e39ebac97afadd5b3311c54788472
Chris Henry <cdhenry@gmail.com>
2018-04-14 14:58:06 +0000

    fixed some linking errors in layouts and added css to welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def edit
-  end
-
   def create
     user = User.find_by(email: params[:email])
     if user = user.try(:authenticate, params[:email])
@@ -12,14 +9,6 @@ class SessionsController < ApplicationController
       redirect_to user_path(user)
     else
       redirect_to signin_path
-    end
-  end
-
-  def update
-    if current_user.update
-      redirect_to user_path(current_user)
-    else
-      render :edit
     end
   end
 
