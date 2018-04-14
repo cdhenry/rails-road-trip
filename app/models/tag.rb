21a43190aa9bf15f9fcae94f1e490a37609393a4
Chris Henry <cdhenry@gmail.com>
2018-04-14 19:52:35 +0000

    added tags index page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,17 @@ class Tag < ActiveRecord::Base
   has_many :destination_tags
   has_many :destinations, through: :destination_tags
 
+  def self.popularity_ordered
+    joins(:destination_tags).group("destination_tags.tag_id").order("COUNT(destination_tags.tag_id) DESC")
+  end
+
+  def times_used
+    self.destinations.count
+  end
+
+  def last_destination
+    if last_association = DestinationTag.where(tag_id: self.id).order(created_at: :desc).limit(1).first
+      Destination.find(last_association.destination_id)
+    end
+  end
 end
