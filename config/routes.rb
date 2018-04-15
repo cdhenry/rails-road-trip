b8292769580220e9b2878f43d766adca45c0cb8c
Chris Henry <cdhenry@gmail.com>
2018-04-15 00:01:51 +0000

    added routes for user road trips

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :road_trips
   resources :destinations
   resources :tags
+  resources :user_road_trips, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
