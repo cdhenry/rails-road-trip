cafaccf375b0c88743504bea751acdc7474e1918
Chris Henry <cdhenry@gmail.com>
2018-04-16 03:31:55 +0000

    got completed buttons working on road trip show page

--- app/controllers/user_road_trips_controller.rb
+++ app/controllers/user_road_trips_controller.rb
@@ -13,17 +13,16 @@ class UserRoadTripsController < ApplicationController
     elsif trip_status == "In Process" && current_user.current_trip_id
       flash[:error] = "You cannot be on two trips at once"
       redirect_to road_trip_path(trip_id)
-    elsif urt = find_urt
+    elsif urt = find_urt && trip_status == "Completed"
       urt.status = trip_status
-      mark_completed
+      check_completed
       urt.save
-      @user.miles_driven = @user.miles_driven.to_i + RoadTrip.find(params[:user_road_trip][:road_trip_id]).total_miles.to_i
-      @user.save
       redirect_to road_trip_path(trip_id)
     else
       urt = UserRoadTrip.new(user_road_trip_params)
+      check_completed
       urt.save
-      @user.current_trip_id = trip_id
+      @user.current_trip_id = trip_id if trip_status == "In Process"
       @user.save
       redirect_to road_trip_path(trip_id)
     end
@@ -46,9 +45,13 @@ class UserRoadTripsController < ApplicationController
       UserRoadTrip.find_by(road_trip_id: params[:user_road_trip][:road_trip_id], user_id: params[:user_road_trip][:user_id])
     end
 
-    def mark_completed
-      if current_user.current_trip_id == trip_id && trip_status == "Completed"
-        current_user.current_trip_id = nil
+    def check_completed
+      if trip_status == "Completed"
+        @user.miles_driven = @user.miles_driven.to_i + RoadTrip.find(params[:user_road_trip][:road_trip_id]).total_miles.to_i
+        @user.save
+        if @user.current_trip_id == trip_id
+          @user.current_trip_id = nil
+        end
       end
     end
 
