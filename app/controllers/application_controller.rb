99c56b634b4ef469434fe17eaf7d4e17101d0022
Chris Henry <cdhenry@gmail.com>
2018-04-16 17:07:05 +0000

    added error messages if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :require_logged_in
+  helper_method :current_user, :logged_in?
+  before_action :require_login
+  skip_before_action :require_login, only: [:home]
 
   def current_user
     @user = (User.find_by(id: session[:user_id]) || User.new)
@@ -12,7 +14,8 @@ class ApplicationController < ActionController::Base
     current_user.id != nil
   end
 
-  def require_logged_in
+  def require_login
+    flash[:error] = "You must log in to perform this action."
     redirect_to root_path unless logged_in?
   end
 end
