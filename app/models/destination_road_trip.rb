e71e51b17a5c3c14b77aafce06a6647f62a9ae0e
Chris Henry <cdhenry@gmail.com>
2018-04-17 21:29:24 +0000

    modified models for new combined form idea

--- app/models/destination_road_trip.rb
+++ app/models/destination_road_trip.rb
@@ -1,4 +1,8 @@
 class DestinationRoadTrip < ActiveRecord::Base
   belongs_to :destination
   belongs_to :road_trip
+  validates_associated :destination
+  validates_associated :road_trip
+  accepts_nested_attributes_for :road_trip
+  accepts_nested_attributes_for :destination
 end
--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -12,8 +12,8 @@ class RoadTrip < ActiveRecord::Base
   has_many :tags, through: :destinations
 
   accepts_nested_attributes_for :destinations
-  accepts_nested_attributes_for :destination_road_trips
-  accepts_nested_attributes_for :tags
+  #accepts_nested_attributes_for :destination_road_trips
+  #accepts_nested_attributes_for :tags
 
   def author
     User.find(self.author_id)
@@ -33,22 +33,22 @@ class RoadTrip < ActiveRecord::Base
       destination_attribute.delete("stop_order")
       tags = destination_attribute["tags_attributes"].first[1]
       destination_attribute.delete("tags_attributes")
-
+      binding.pry
       destination = Destination.find_or_initialize_by(destination_attribute)
       set_tags(tags, destination)
-
+      binding.pry
       if destination.save
         self.destination_road_trips.create(destination_id: destination.id, destination_order: stop_order)
       end
     end
   end
-
-  def destination_road_trips_attributes=(destination_road_trips_attributes)
-    destination_road_trips_attributes.values.each do |destination_road_trip_attribute|
-      if drt = self.destination_road_trips.find_by(destination_id: destination_road_trip_attribute["destination_id"])
-        drt.destination_order = destination_road_trip_attribute["destination_order"]
-        drt.save
-      end
-    end
-  end
+  #
+  # def destination_road_trips_attributes=(destination_road_trips_attributes)
+  #   destination_road_trips_attributes.values.each do |destination_road_trip_attribute|
+  #     if drt = self.destination_road_trips.find_by(destination_id: destination_road_trip_attribute["destination_id"])
+  #       drt.destination_order = destination_road_trip_attribute["destination_order"]
+  #       drt.save
+  #     end
+  #   end
+  # end
 end
