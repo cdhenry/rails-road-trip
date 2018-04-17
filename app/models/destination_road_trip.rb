1304b87e064e7d7e8d23ee7778eb1269dab98fc2
Chris Henry <cdhenry@gmail.com>
2018-04-17 02:47:24 +0000

    fixed issue with validations

--- app/models/destination_road_trip.rb
+++ app/models/destination_road_trip.rb
@@ -1,9 +1,4 @@
 class DestinationRoadTrip < ActiveRecord::Base
-  validates :destination_id, presence: true
-  validates :road_trip_id, presence: true
-  validates :road_trip_id, uniqueness: { scope: :destination_id,
-    message: "A destination can be associated with a trip only once."}
-
   belongs_to :destination
   belongs_to :road_trip
 end
--- app/models/destination_tag.rb
+++ app/models/destination_tag.rb
@@ -1,9 +1,4 @@
 class DestinationTag < ActiveRecord::Base
-  validates :destination_id, presence: true
-  validates :tag_id, presence: true
-  validates :destination_id, uniqueness: { scope: :tag_id,
-    message: "A tag can be associated with a destination only once."}
-
   belongs_to :destination
   belongs_to :tag
 end
